3aedf7a00a9e77de73536da667d9cddb123ee729
Marellisiddu <148313520+Marellisiddu@users.noreply.github.com>
2026-03-17 13:52:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from groq import Groq
+
+# Title
+st.title("AI Chatbot")
+
+# Groq API
+client = Groq(api_key="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")
+
+# User input
+user_input = st.text_input("Who is the no.1 actor in tollywood?")
+
+# Generate response
+if user_input:
+    completion = client.chat.completions.create(
+        model="openai/gpt-oss-120b",
+        messages=[
+            {"role": "user", "content": user_input}
+        ],
+        temperature=1,
+        max_tokens=1024
+    )
+
+    response = completion.choices[0].message.content
+
+    st.write("🤖 Bot:", response)
